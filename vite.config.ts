3b70cdde01fb31114478a460d114cdcb184e33b5
pathusharma47-collab <pathusharma47@gmail.com>
2026-08-14 18:14:15 +0530

    5th c

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart({
-      server: {
-        preset: "vercel",
-      },
-    }),
+    tanstackStart(),
     react(),
     tailwindcss(),
   ],
